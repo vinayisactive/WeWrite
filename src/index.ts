c5d3d391ccd76338d140cb94b0637a4ac5b21d61
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-08-28 12:15:50 +0530

    Routes ,Routes Structure and middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
+import { singinRoute, singupRoute } from './routes/authRoute';
+import blogRoutes from './routes/blog';
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route("api/v1/singup", singupRoute); 
+app.route("api/v1/signin", singinRoute); 
+app.route("api/v1/blog", blogRoutes)
 
-export default app
+export default app;
---
+++ src/routes/blog.ts
@@ -0,0 +1,32 @@
+import { Hono } from "hono";
+
+const blogRoutes = new Hono(); 
+
+blogRoutes.post("/", async({req, res}) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}); 
+
+
+
+blogRoutes.get("/", async({req, res}) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}); 
+
+
+blogRoutes.patch("/", async({req, res}) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}); 
+
+export default blogRoutes; 
