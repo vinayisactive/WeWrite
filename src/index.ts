fdaa10e7b82b54ff8ad620c8098abd4613148c15
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-08-29 17:33:19 +0530

    Updated index file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from 'hono'
 import { apiV1Router } from './routes';
+import { Bindings } from './types/interfaces';
 
-const app = new Hono()
+const app = new Hono<{Bindings: Bindings}>()
+
 app.route("/api/v1", apiV1Router); 
 
 export default app;
