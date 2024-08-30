537cedc1b09964438c0700487c56de07ee3837e6
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-08-30 09:38:01 +0530

    Zod validation for signin body

--- src/types/zod.ts
+++ src/types/zod.ts
@@ -1,7 +1,12 @@
-import { z } from 'zod'
+import { string, z } from 'zod'
 
 export const signupSchema = z.object({
     username: z.string(),
     email: z.string().email(),
     password: z.string().min(6)
+}); 
+
+export const singinSchema = z.object({
+    email: z.string().email(),
+    password: string()
 }); 
