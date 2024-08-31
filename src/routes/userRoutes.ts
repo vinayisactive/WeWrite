6425eebfe5ca10741b2b436c7dfbfa3b3ffb42d0
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-08-31 18:46:02 +0530

    Updated routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,13 @@
 import { Hono } from "hono";
 import { Bindings } from "../types/interfaces";
-import { getUser, getUserById, updateUserDetails } from "../controllers/user.controllers";
+import { deleteUser, getUser, getUserById, updateUserDetails } from "../controllers/user.controllers";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
 const userRoutes = new Hono<{ Bindings : Bindings }>(); 
 
 userRoutes.get("/", authMiddleware, getUser); 
 userRoutes.get("/:id", getUserById); 
-userRoutes.patch("/:id", authMiddleware, updateUserDetails); 
+userRoutes.patch("/", authMiddleware, updateUserDetails); 
+userRoutes.delete("/", authMiddleware, deleteUser);
 
 export default userRoutes; 
