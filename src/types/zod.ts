3f01e500c7da7886dde6f7952342a2c47a2c7d5c
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-08-31 16:41:33 +0530

    Zod schemas added for blog creation and updation

--- src/types/zod.ts
+++ src/types/zod.ts
@@ -1,12 +1,45 @@
 import { string, z } from 'zod'
 
+
 export const signupSchema = z.object({
-    username: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6)
+    username: z.string().trim(),
+    email: z.string().email().trim(),
+    password: z.string().min(6).trim()
 }); 
 
 export const singinSchema = z.object({
-    email: z.string().email(),
-    password: string()
+    email: z.string().email().trim(),
+    password: string().trim()
+}); 
+
+export const blogSchema = z.object({
+    title: z.string().min(1).trim(),
+    description: z.string().trim(),
+    content: z.string().min(1).trim(),
+    category: z.enum([
+        "AI",
+        "TECH",
+        "FASHION",
+        "HEALTH",
+        "EDUCATION",
+        "NEWS",
+        "FOOD",
+      ]),
+      published: z.boolean().default(false),
+}); 
+
+export const blogUpdateSchema = z.object({
+    title: z.string().min(1).trim().optional(),
+    description: z.string().trim().optional(),
+    content: z.string().min(1).trim().optional(),
+    category: z.enum([
+        "AI",
+        "TECH",
+        "FASHION",
+        "HEALTH",
+        "EDUCATION",
+        "NEWS",
+        "FOOD",
+      ]).optional(),
+      published: z.boolean().default(false).optional(),
 }); 
